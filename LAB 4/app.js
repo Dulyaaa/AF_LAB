0c86bcae73d83c02ebb3d0f489a07aef1dfd6605
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2021-03-29 16:22:27 +0530

    Add Lab 4

---
+++ LAB 4/app.js
@@ -0,0 +1,65 @@
+{/* <<<<<<<<< Question 01 >>>>>>>>>> */ }
+console.log('Hello World');
+
+{/* <<<<<<<<< Question 02 >>>>>>>>>> */ }
+//Use OS system library
+const os = require('os');
+console.log('Architecture ' + os.arch());
+console.log('CPUs ' + os.cpus().length);
+console.log('OS ' + os.platform());
+
+{/* <<<<<<<<< Question 03 >>>>>>>>>> */ }
+//Read a file
+const fs = require('fs');
+const fileName = __dirname + '/test.txt';
+fs.readFile(fileName, (err, data) => {
+    if (err) {
+        console.err(err);
+    }
+
+    console.log(data.toString());
+});
+
+const data1 = fs.readFileSync(fileName);
+console.log(data1.toString());
+
+{/* <<<<<<<<< Question 04 >>>>>>>>>> */ }
+const fileName1 = __dirname + '/test.txt';
+const outFileName = __dirname + '/test-copy.txt';
+
+const readStream = fs.createReadStream(fileName1);
+const writeStream = fs.createWriteStream(outFileName);
+
+readStream.pipe(writeStream);
+
+readStream.on('data', data => {
+    console.log(data.toString());
+});
+
+{/* <<<<<<<<< Question 05 >>>>>>>>>> */ }
+
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// }).listen(3000);
+
+const http = require('http');
+http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    switch (req.method) {
+        case 'GET':
+            res.write('<h1>Hello World</h1>');
+            res.end();
+            break;
+        case 'POST':
+            req.on('data', data => {
+                res.write('<h1>Hello ' + data + '</h1>');
+                res.end();
+            });
+            break;
+    }
+}).listen(3000, (err) => {
+    console.log('Server is listening to port 3000')
+});
